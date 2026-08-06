cb049849e0b6be26d9b03545fcf93babe05662ba
drmonro <rapman477@gmail.com>
2026-08-07 05:07:03 +0700

    Update Contact Email Address
    - Replace the public email address in company information
    - Send website inquiries to the updated mailbox

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ if (isset($_GET["exit"])) {
 				js("$.notification.show('error','Bot!')");
 			} else {
 				$m = 'Пользователь '.trim($_POST["memail"]).' отправил сообщение/вопрос с сайта: '.$m;
-				send_mail("uchetokk@mail.ru", $m);
+				send_mail("ooo_okk@mail.ru", $m);
 				unset($_SESSION['captcha']);
 			}
 		}
